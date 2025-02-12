610634f4ff52caf9dc2efc9eb7bcc2a5ec9db9c0
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-02-12 18:29:41 +0200

    Add files via upload

---
+++ 06.02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calc1 = new Calculate();
+            Calculate calc2 = new Calculate(4);
+            Calculate calc3 = new Calculate(3, 12);
+
+            Console.WriteLine(calc1);
+            Console.WriteLine(calc2);
+            Console.WriteLine(calc3);
+
+            Console.WriteLine("Корень из произведения: " + new Func<double>(() => Math.Sqrt(calc3.num1 * calc3.num2))());
+            Console.ReadKey();
+        }
+        class Calculate
+        {
+            public double num1;
+            public double num2;
+
+            // Конструктор без параметров
+            public Calculate() : this(0, 0) { }
+
+            // Конструктор с одним параметром
+            public Calculate(double num) : this(num, num) { }
+            public Calculate(double num1, double num2)
+            {
+                this.num1 = num1;
+                this.num2= num2;
+            }
+
+            // Функция формирования строки с информацией об объекте
+            public override string ToString()
+            {
+                return $"Num1: {num1}, Num2: {num2}";
+            }
+
+            // Функция вычисления корня квадратного из произведения чисел
+            public double CalculateRoot()
+            {
+                return Math.Sqrt(num1 * num2);
+            }
+        }
+
+    }
+}

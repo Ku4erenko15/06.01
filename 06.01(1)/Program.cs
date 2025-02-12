4a3d55f783a1e7dfb49c29a899585c1318bedcb7
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-02-12 18:38:33 +0200

    Add files via upload

---
+++ 06.01(1)/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._01_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.Name = "Алексей";
+            person1.Age = 25;
+            person1.DisplayInfo();
+
+            Person person2 = new Person { Name = "Мария", Age = 30 };
+            person2.DisplayInfo();
+
+            Console.Write("Введите имя: ");
+            string inputName = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            int inputAge = int.Parse(Console.ReadLine());
+
+            Person person3 = new Person(inputName, inputAge);
+            person3.DisplayInfo();
+            Console.ReadKey();
+        }
+
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Person()
+            {
+                Name = "Неизвестно";
+                Age = 0;
+            }
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Имя: {Name}, Возраст: {Age}");
+            }
+        }
+    }
+}
